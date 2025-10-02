718af088d3455ae2fec3f7f786a4ade6e674cc56
hasdajustin <matheusjustinhasda@gmail.com>
2025-10-02 06:36:53 +0600

    register donor for access admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Donor
 
 # Register your models here.
+@admin.register(Donor)
+class DonorAdmin(admin.ModelAdmin):
+    list_display = ("full_name", "age", "blood_group",)
+    list_filter = ("gender", "blood_group", "created_at")
+    search_fields = ("full_name", "phone", "email")
+    ordering = ("-created_at",)
